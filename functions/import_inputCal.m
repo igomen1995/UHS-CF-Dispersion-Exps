95176b1b3dae0c20bcf244af6ab296d884f4706e
izg5132 <izg5132@E2-EME-HSDT438>
2026-03-05 10:05:39 -0500

    Added description

--- functions/import_inputCal.m
+++ functions/import_inputCal.m
@@ -1,5 +1,8 @@
 function data_out = import_inputCal(input_name_xlsx)
+
 %IMPORT_INPUTCAL Summary of this function goes here
+% import fields for importing data of calibration experiments
+
 opts = spreadsheetImportOptions("NumVariables", 16);
 % Specify sheet and range
 opts.Sheet = "Sheet1";
@@ -9,9 +12,11 @@ opts.VariableNames = ["Key", "Date", "Fluid1", "T_C", "P_psig", "Q_mlmin", "Run"
 opts.VariableTypes = ["string", "string","string", "double", "string", "string", "double", "datetime", "datetime", "double", "string", "string", "string", "string", "string", "string", "string"];
 data_out = readtable(input_name_xlsx,opts);
 
+% Specify time format which is same for all times in different data sets
 data_out.st = datetime(data_out.st,'Format','MM/dd/uuuu HH:mm:ss');
 data_out.et = datetime(data_out.et,'Format','MM/dd/uuuu HH:mm:ss');
 
+% Data of P and Q in one cell as string is converted to arrays
 P_cell = cell(height(data_out),1);
 Q_cell = cell(height(data_out),1);
 for i = 1:height(data_out)
