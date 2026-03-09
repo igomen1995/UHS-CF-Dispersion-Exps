0c2bbb8aba6cfd8f28f35a6e5ee6c337cd90cad9
izg5132 <izg5132@E2-EME-HSDT438>
2026-03-09 13:38:30 -0400

    also part of changes made in main Data Extract

---
+++ functions/import_inputExp.m
@@ -0,0 +1,27 @@
+function data_out = import_inputExp(input_name_xlsx)
+
+%IMPORT_INPUTCAL Summary of this function goes here
+% import fields for importing data of experiments
+
+% Do not change unless input excel format changed
+
+opts = spreadsheetImportOptions("NumVariables", 32);
+% Specify sheet and range
+opts.Sheet = "Sheet1";
+opts.DataRange = [3,Inf];
+% Specify column names and types
+opts.VariableNames = ["Key", "Date", "Type","Fluid1", "Fluid2", ...
+    "T", "P", "Q", "Run", "D", "L", "phi", "K", "Vcore", ...
+    "setupVersion", "Vlinesbefore", "Vlinesafter", "Vtotal", "Comments", "workingPump", "cushionPump", "confiningPump", "st", "et", "dt", ...
+    "path", "pumps_data_name", "trans_data_name", "MFM_data_name", "PGD1_data_name", "PGD2_data_name","GMT_PGD"];
+opts.VariableTypes = ["string", "string","string", "string", "string", ...
+    "double", "double", "double", "double", "double", "double", "double", "double", "double", ...
+    "string", "double", "double", "double","string", "double","double","double","datetime", "datetime", "double", ...
+    "string", "string", "string", "string", "string", "string", "string"];
+data_out = readtable(input_name_xlsx,opts);
+
+data_out.st = datetime(data_out.st,'Format','MM/dd/uuuu HH:mm:ss');
+data_out.et = datetime(data_out.et,'Format','MM/dd/uuuu HH:mm:ss');
+
+end
+
